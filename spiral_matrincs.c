ff6ade336c36e931e2661db0935d7d4dbb6091e9
adhithyankrishna <85678439+adhithyankrishna@users.noreply.github.com>
2022-09-21 14:58:35 +0530

    Update spiral_matrincs.c

--- spiral_matrincs.c
+++ spiral_matrincs.c
@@ -1,42 +1,48 @@
 #include<stdio.h>
 
 void main(){
-    int mat[4][4] = 
-    {{1, 2, 3, 4},
-    { 5, 6, 7, 8},
-    { 9, 10,11,12},
-    { 13,14,15,16}};
-    int i,j,count = 4;
-    int temp1 = 0,temp2 = 0,r = 0,c = 0;
-
+    int mat[6][6] = 
+    {{1, 2, 3, 4,61,62},
+    { 5, 6, 7, 8,63,64},
+    { 9, 10,11,12,65,66},
+    { 13,14,15,1667,68,99},{
+      68,69,70,71,72,77},
+     { 91,92,93,94,95,96
+    }};
+    int i,j,count = 6;
+    int temp1 = 0,temp2 = 0,r = 0,c = 0,t = 0,l = 0;
+    
     while(temp1!=count && temp2!=count){
-        for ( i = 0; i < count - temp1; i++)
+        for ( i = l; i < count - temp1; i++)
         {
-            printf("\t%d",mat[r][i]);
-            printf("\nfrom 1 %d %d",r,i);
+            printf("\t%d",mat[t][i]);
+            //printf("\nfrom 1 %d %d",r,i);
         }
-        for ( j = 1; j < count - temp2; j++)
+    
+        for ( j = 1+l; j < count - temp2; j++)
         {
             printf("\t%d",mat[j][i-1]);
-            printf("\nfrom 2 %d %d",j,i-1);
+            //printf("\nfrom 2 %d %d",j,i-1);
         }
         r = j-1;
-        
         //temp2++;
+
         for ( i = r-1; i >=temp1; i--)
         {
             printf("\t%d",mat[r][i]);
-            printf("\nfrom 3 %d %d",r,i);
+            //printf("\nfrom 3 %d %d",r,i);
         }
-        c = i;
+        c = i+1;
+
         for ( i = r-1; i >temp2; i--)
         {
             printf("\t%d",mat[i][c]);
-            printf("\nfrom 4 %d %d",i,c);
+            //printf("\nfrom 4 %d %d",i,c);
         }
         temp1++;
         temp2++;
-        
+        t++;
+        l++;
         
     }
 
